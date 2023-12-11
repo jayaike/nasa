a9f50679df74544f351c92942f76d2ae9b08cb8b
kayakthedev <nsjcorps@gmail.com>
2023-12-11 11:37:50 +0000

    Update index.py

--- api/index.py
+++ api/index.py
@@ -8,7 +8,7 @@ def home():
     loaded_model = pickle.load(open('model.sav', 'rb'))
     
     response = app.response_class(
-        response=json.dumps({'isHazardous': True, 'm': str(loaded_model.predict([1.198271, 2.679415, 13569.249224, 54839740,	16.73]))}),
+        response=json.dumps({'isHazardous': True, 'm': str(loaded_model.predict([[1.198271, 2.679415, 13569.249224, 54839740,	16.73]]))}),
         status=200,
         mimetype='application/json'
     )
