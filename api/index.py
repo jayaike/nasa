49e7b4f193db2a82a59884074ac1473e99a6952c
kayakthedev <nsjcorps@gmail.com>
2023-12-11 11:35:47 +0000

    Update index.py

--- api/index.py
+++ api/index.py
@@ -6,8 +6,9 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     loaded_model = pickle.load(open('model.sav', 'rb'))
+    
     response = app.response_class(
-        response=json.dumps({'isHazardous': True, 'm': str(loaded_model)}),
+        response=json.dumps({'isHazardous': True, 'm': str(loaded_model.predict([1.198271, 2.679415, 13569.249224, 54839740,	16.73]))}),
         status=200,
         mimetype='application/json'
     )
