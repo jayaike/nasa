3e35be7445b4c8253fb057e2c4457cae9fd7dc5d
kayakthedev <nsjcorps@gmail.com>
2023-12-11 10:56:10 +0000

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,7 +1,12 @@
-from flask import Flask
+from flask import Flask, json
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'Hello, World!'
+    response = app.response_class(
+        response=json.dumps({'isHazardous': True}),
+        status=200,
+        mimetype='application/json'
+    )
+    return response
