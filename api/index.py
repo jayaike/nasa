426391356abf9f5627b3355da0363e58bd93e823
kayakthedev <nsjcorps@gmail.com>
2023-12-11 11:50:53 +0000

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,12 +1,18 @@
 from flask import Flask, json
+from sklearn.preprocessing import MinMaxScaler
 import pickle
+
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     loaded_model = pickle.load(open('model.sav', 'rb'))
-    prediction = loaded_model.predict([[0.255009, 0.570217, 42737.733765, 4.627557e+07, 20.09]])[0]
+
+    X = [[0.255009, 0.570217, 42737.733765, 4.627557e+07, 20.09]]
+    scaler = MinMaxScaler()
+    X_scaled = scaler.fit_transform(X)    
+    prediction = loaded_model.predict(X_scaled)[0]
    
     response = app.response_class(
         response=json.dumps({'isHazardous': bool(prediction)}),
