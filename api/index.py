f8e699f2fefe622639ff103b8c5915b19e79a785
kayakthedev <nsjcorps@gmail.com>
2023-12-11 12:27:36 +0000

    fit and transform scaler

--- api/index.py
+++ api/index.py
@@ -2,16 +2,26 @@ from flask import Flask, json
 from sklearn.preprocessing import MinMaxScaler
 import pickle
 import numpy as np
+import pandas as pd
 
 app = Flask(__name__)
+
+df = pd.read_csv('neo.csv')
+df['hazardous'] = df['hazardous'].replace({False: 0, True: 1})
+
+X = df.drop("hazardous", axis=1)
+y = df["hazardous"]
+
+scaler = MinMaxScaler()
+scaler.fit(X)
 
 @app.route('/')
 def home():
     loaded_model = pickle.load(open('model.sav', 'rb'))
 
     X = np.array([[0.265800, 0.594347, 73588.726663, 6.143813e+07, 20.00]])
-    scaler = MinMaxScaler()
-    X_scaled = scaler.fit_transform(X)   
+    scaler = MinMaxScaler(feature_range=())
+    X_scaled = scaler.transform(X)   
     prediction = loaded_model.predict(X_scaled)[0]
    
     response = app.response_class(
