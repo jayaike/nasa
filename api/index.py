050a829bf036144c37c2a9dfabce3fa04cb46b2a
kayakthedev <nsjcorps@gmail.com>
2023-12-11 11:12:34 +0000

    load model

--- api/index.py
+++ api/index.py
@@ -1,11 +1,13 @@
 from flask import Flask, json
+import pickle
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
+    loaded_model = pickle.load(open('model.sav', 'rb'))
     response = app.response_class(
-        response=json.dumps({'isHazardous': True}),
+        response=json.dumps({'isHazardous': True, 'm': str(loaded_model)}),
         status=200,
         mimetype='application/json'
     )
